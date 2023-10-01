fd5cc02efa9282461d30cdf3070b7f75737af86a
Ash Lynn <Ash Lynn@DESKTOP-387E9E8>
2023-10-01 16:20:04 -0700

    Add project files.

---
+++ CreditFinder/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace CreditFinder.Models
+{
+    public class User
+    {
+        public string Username {  get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
---
+++ CreditFinder/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CreditFinder.Models
+{
+    public class UserDto
+    {
+        public required string Username { get; set; } = string.Empty;
+        public required string Password { get; set; } = string.Empty;
+    }
+}
